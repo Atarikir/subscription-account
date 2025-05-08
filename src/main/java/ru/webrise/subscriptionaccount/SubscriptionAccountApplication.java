19002a83e734ad7649ab71488e8d360f451f514a
Dmitriy Kirillov <mitkiril@gmail.com>
2025-05-08 10:07:02 +0300

    added docker

---
+++ src/main/java/ru/webrise/subscriptionaccount/SubscriptionAccountApplication.java
@@ -0,0 +1,12 @@
+package ru.webrise.subscriptionaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionAccountApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SubscriptionAccountApplication.class, args);
+  }
+}
